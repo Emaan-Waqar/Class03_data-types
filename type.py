3666a2668e392f4b201e14ab111c87c19209fc32
Emaan Waqar <emaanwaqar0102@gmail.com>
2025-07-10 13:43:36 +0500

    Data types

---
+++ type.py
@@ -0,0 +1,13 @@
+#checking the data types of different  values
+
+a=16
+print("type of a is: ", type((a)))
+
+b=4.5
+print("type of b is: ", type((b)))
+
+c= "coding"
+print("type of c is: ", type((c)))
+
+d= True
+print("type of d is: ", type((d)))
